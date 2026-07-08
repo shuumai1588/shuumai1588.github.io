f8bfa7315f5d38937fda59123c5cc421bc106af6
しぐねる <shuumai.jp@gmail.com>
2026-07-08 18:24:40 +0900

    Update layout-loader.js

--- src/core/layout-loader.js
+++ src/core/layout-loader.js
@@ -23,7 +23,6 @@ async function overrideMultipleParts(url, targets) {
 }
 
 overrideMultipleParts("src/core/layout.html", [
-  { selector: "head",   overrideId: "override-head" },
   { selector: "header", overrideId: "override-header" },
   { selector: "footer", overrideId: "override-footer" }
 ]);
